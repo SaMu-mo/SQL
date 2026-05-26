60254fed5e2100b8603d60093fe7e822ad710749
SaMu-mo <menesesorozcosamuel@gmail.com>
2026-05-26 10:23:43 -0500

    Crear Tabla

---
+++ Sentencisa SQL.sql
@@ -0,0 +1,8 @@
+CREATE TABLE estudiantes (
+    id_estudiante  INT,
+    nombres        VARCHAR(50),
+    apellidos      VARCHAR(50),
+    edad           INT,
+    curso          VARCHAR(50),
+    fecha_registro VARCHAR(10)
+);
